9dbad0876098a53986f46eac97838770f1732d09
Shardul Dhiwar <sujaldhiwar007@gmail.com>
2025-07-21 14:01:16 +0530

    Minor changes

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,51 +1,50 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
-import JobListing from './JobListing';  
+import React, { useState, useEffect } from 'react';
+import JobListing from './JobListing';
 import Spinner from './Spinner';
 
-const JobListings = ({isHome = false}) => {
-   const [jobs, setJobs]= useState ([]);
-   const [loading, setLoading]= useState(true);
+const JobListings = ({ isHome = false }) => {
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-   useEffect(() => {
-    const fetchJobs=async () => {
-      const apiUrl = isHome 
-      ? '/api/jobs?_limit=3'
-      : '/api/jobs'
-      try{
-      const res = await fetch(apiUrl);
-      const data = await res.json();
-      setJobs(data);
-      }
-      catch(error){
-        console.log('Error Fetching Data', error);
-      }finally{
+  useEffect(() => {
+    const fetchJobs = async () => {
+      const apiUrl = '/jobs.json'; // static file in public folder
+      try {
+        const res = await fetch(apiUrl);
+        const data = await res.json();
+
+        // if on homepage, show only 3 jobs
+        const jobsToShow = isHome ? data.jobs.slice(0, 3) : data.jobs;
+        setJobs(jobsToShow);
+      } catch (error) {
+        console.error('Error Fetching Data', error);
+      } finally {
         setLoading(false);
       }
-    }
+    };
+
     fetchJobs();
-   }, []);
+  }, [isHome]);
 
   return (
     <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
         <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-        { isHome ? 'Recent Jobs' : 'Browse Jobs'}
+          {isHome ? 'Recent Jobs' : 'Browse Jobs'}
         </h2>
-        
-          {loading?(<Spinner loading={loading}/>):(
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            {jobs.map((job) => (
-              <JobListing key={job.id} job={job}/>
-             ))}
-             </div>
-          )}
-          
 
-        
+        {loading ? (
+          <Spinner loading={loading} />
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {jobs.map((job) => (
+              <JobListing key={job.id} job={job} />
+            ))}
+          </div>
+        )}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default JobListings
+export default JobListings;
